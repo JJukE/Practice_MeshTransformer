42840afed5e06349c87ea741b66dba15966a4a17
JJuk_E <psj9156@naver.com>
2022-08-16 19:49:08 +0900

    mesh reconstruction experiment (failed)

---
+++ HumanBodyRecon.sh
@@ -0,0 +1,3 @@
+python ./metro/tools/end2end_inference_bodymesh.py \
+--resume_checkpoint ./models/metro_release/metro_3dpw_state_dict.bin \
+--image_file_or_path ./samples/human-body
---
+++ HumanHandRecon.sh
@@ -0,0 +1,3 @@
+python ./metro/tools/end2end_inference_handmesh.py \
+--resume_checkpoint ./models/metro_release/metro_hand_state_dict.bin \
+--image_file_or_path ./samples/hand
